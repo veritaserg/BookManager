d83bd44995af6c80596054b2915bbf43c314b2b8
veritaserg <infophotomasterscomua@gmail.com>
2018-12-03 22:35:57 +0200

    update

---
+++ src/main/java/dao/BookDao.java
@@ -0,0 +1,21 @@
+package main.java.dao;
+
+import main.java.model.Book;
+import java.util.Collection;
+import java.util.List;
+
+public interface BookDao {
+
+    void addBook(Book book);
+
+    void updateBook(Book book);
+
+    List<Book> listBooks();
+
+    Book getBooksById(int id);
+
+    void removeBook(int id);
+
+    Collection<Book> getBooks(String search);
+
+}
